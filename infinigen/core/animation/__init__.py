9cdaae056a79e30583b7abbe271ac12605efa198
crypticsymmetry <ccbb121@gmail.com>
2026-03-16 08:17:06 -0700

    Add indoor human animation policy with semantic scoring

---
+++ infinigen/core/animation/__init__.py
@@ -0,0 +1,5 @@
+"""Animation policy utilities for indoor human motion selection."""
+
+from .policy_human import HumanAnimationPolicy, MocapClipCandidate, PolicyDecision
+
+__all__ = ["HumanAnimationPolicy", "MocapClipCandidate", "PolicyDecision"]
